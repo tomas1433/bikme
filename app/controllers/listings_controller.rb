65cfe9d697a6faf586884dc4cc7e4c6f3793c7b8
tomas1433 <t.tourlet@gmail.com>
2016-09-14 11:03:23 +0000

    add update and destroy action + refacto controller avec 1 partial

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,15 +1,17 @@
 class ListingsController < ApplicationController
+before_action :find_listing, only: [:show, :edit, :update, :destroy]
   def index
      @listings = Listing.all
 
   end
-  def show
-    @listing = Listing.find(params[:id])
-  end
 
-  def new
-   @listing = Listing.new
-  end
+    def show
+
+    end
+
+    def new
+     @listing = Listing.new
+    end
 
   def create
     @listing = Listing.new(listing_params)
@@ -19,13 +21,33 @@ class ListingsController < ApplicationController
       render :new
     end
   end
+
   def edit
+
   end
-  def update
-  end
+
+      def update
+
+        if @listing.update(listing_params)
+          redirect_to listings_path
+          else
+          render :edit
+        end
+      end
+
   def destroy
+
+    @listing.destroy
+redirect_to listings_path
   end
+
+private
+
   def listing_params
     params.require(:listing).permit(:name, :description)
+  end
+
+  def find_listing
+  @listing = Listing.find(params[:id])
   end
 end
